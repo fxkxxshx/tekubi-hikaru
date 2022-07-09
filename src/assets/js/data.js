833d0d437f3cdb6b7688d3eb61f6a97a7a6b348f
fkxsh <fxkxxshx@gmail.com>
2022-07-10 03:27:28 +0900

    data.js 追加

---
+++ src/assets/js/data.js
@@ -0,0 +1,57 @@
+const data = [
+    {
+        params: {
+            group: 'aqours',
+        },
+        props: {
+            title: 'Aqours',
+            color: [
+                '#FF791B',
+                '#FF7777',
+                '#00D29E',
+                '#F43232',
+                '#2AA4DB',
+                '#AEAEAE',
+                '#CFBA0F',
+                '#A530E0',
+                '#EE55B7',
+            ]
+        }
+    },
+    {
+        params: {
+            group: 'liella-5',
+        },
+        props: {
+            title: 'Liella!（5人ver.）',
+            color: [
+                '#FF7F27',
+                '#A0FFF9',
+                '#FF6E90',
+                '#74F466',
+                '#0000A0',
+            ]
+        }
+    },
+    {
+        params: {
+            group: 'liella-9',
+        },
+        props: {
+            title: 'Liella!（9人ver.）',
+            color: [
+                '#FF7F27',
+                '#A0FFF9',
+                '#FF6E90',
+                '#74F466',
+                '#0000A0',
+                '#FFF442',
+                '#FF3535',
+                '#B2FFDD',
+                '#FF51C4',
+            ]
+        }
+    },
+]
+
+export { data }
